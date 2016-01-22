e10ce729480e9860dbdbc68185758ce6918436b7
Vallecera, Chejei <chejei.vallecera@gmail.com>
2016-01-22 16:05:24 +0800

    new updates

---
+++ PriceWise/www/js/send-email.js
@@ -0,0 +1,27 @@
+// Create a function to log the response from the Mandrill API
+function log(obj) {
+    $('#response').text(JSON.stringify(obj));
+}
+
+// create a new instance of the Mandrill class with your API key
+var m = new mandrill.Mandrill('IHNhkCwvtG94atpLU836zA');
+
+// create a variable for the API call parameters
+var params = {
+    "message": {
+        "from_email":"your_email_address",
+        "to":[{"email":"your_recipient_address"}],
+        "subject": "Sending a text email from the Mandrill API",
+        "text": "I'm learning the Mandrill API at Codecademy."
+    }
+};
+
+function sendTheMail() {
+// Send the email!
+
+    m.messages.send(params, function(res) {
+        log(res);
+    }, function(err) {
+        log(err);
+    });
+}
